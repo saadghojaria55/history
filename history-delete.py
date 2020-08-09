48425c610e9cd640c078676a47e6dbb5b139c2ef
Saad Ghojaria <ghojariasaad@gmail.com>
2020-08-09 20:00:41 +0530

    Update history-delete.py

--- history-delete.py
+++ history-delete.py
@@ -2,7 +2,7 @@ import sqlite3
 conn = sqlite3.connect("C:/Users/LENOVO/AppData/Local/Google/Chrome/User Data/Default/History") 
   
 # point out at the cursor 
-c = conn.cursor() 
+c1 = conn.cursor() 
 id = 0  
    
 result = True
@@ -10,14 +10,14 @@ while result:
       
     result = False
     ids = [] 
-    for rows in c.execute("SELECT id,url FROM urls WHERE url LIKE '%geeksforgeeks%'"): 
+    for rows in c1.execute("SELECT id,url FROM urls WHERE url LIKE '%geeksforgeeks%'"): 
           
         print(rows) 
           
         id = rows[0] 
           
         ids.append((id,)) 
-    c.executemany('DELETE from urls WHERE id = ?',ids) 
+    c1.executemany('DELETE from urls WHERE id = ?',ids) 
     conn.commit() 
       
 conn.close() 
